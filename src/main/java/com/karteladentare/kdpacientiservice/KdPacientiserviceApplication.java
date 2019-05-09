a9e05badc7fd7d49ecbae93daa85e6ed4724bc40
Granit Krasniqi <krasniqigranit59@gmail.com>
2019-05-10 00:59:55 +0200

    first commit

---
+++ src/main/java/com/karteladentare/kdpacientiservice/KdPacientiserviceApplication.java
@@ -0,0 +1,13 @@
+package com.karteladentare.kdpacientiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdPacientiserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdPacientiserviceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/karteladentare/kdpacientiservice/domain/Pacienti.java
@@ -0,0 +1,8 @@
+package com.karteladentare.kdpacientiservice.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Pacienti {
+
+}
